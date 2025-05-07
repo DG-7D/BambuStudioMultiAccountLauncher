7918a178a39f8cc436a5be2caa0f8e3fcecd18a2
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-05-07 22:31:56 +0900

    README

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::process;
 use bambu_launcher::Config;
 
 fn main() {
+    println!("Bambu Studio Multi-Account Launcher v{}", env!("CARGO_PKG_VERSION"));
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("ERROR: Invalid syntax. {}", err);
         process::exit(1);
